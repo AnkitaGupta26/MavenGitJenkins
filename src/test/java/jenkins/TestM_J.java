decb8acfa5821e03567e476d4efe5115a348664d
Ankita Gupta <ankgupta29@publicisgroupe.net>
2019-01-23 17:07:05 +0530

    running on remote driver

--- src/test/java/jenkins/TestM_J.java
+++ src/test/java/jenkins/TestM_J.java
@@ -1,22 +1,33 @@
 package jenkins;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class TestM_J {
 
 	
 	@Test
-	public void TestMaven() throws InterruptedException{
+	public void TestMaven() throws InterruptedException, MalformedURLException{
 		System.out.println("Welcome to Jenkins and Maven integration");
 		
-		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/geckodriver.exe");
+		/*System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/geckodriver.exe");
 		
-		WebDriver driver=new FirefoxDriver();
+		WebDriver driver=new FirefoxDriver();*/
+		
+		
+		RemoteWebDriver driver = null; 
+		DesiredCapabilities dc = DesiredCapabilities.firefox(); 
+		driver = new RemoteWebDriver(new URL("http://10.151.158.81:5555/wd/hub"), dc);
+
 		
 		driver.manage().window().maximize();
 		
